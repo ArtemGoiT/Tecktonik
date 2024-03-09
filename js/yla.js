db4b961b78c508409b70977a80ce50fcd081a0ba
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-03-09 23:46:58 +0100

    eee

---
+++ js/yla.js
@@ -0,0 +1,36 @@
+// Первый кодевар на пример меню красного цвета
+
+
+// const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+//
+// const listWithClass = document.querySelector('.menu');
+// console.log(listWithClass);
+//
+// const menuItemsByClass = document.querySelectorAll("li");
+// console.log(menuItemsByClass);
+//
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = 'tomato';
+// console.log(firstMenuItem);
+
+
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = document.querySelector(".list");
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const lastListItem = list.lastElementChild;
+console.log(lastListItem);
+
+const listItems = list.children;
+console.log(listItems);
+
